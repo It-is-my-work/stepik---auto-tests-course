2317e69d5da8b6351eb065d93de42b7921a7b66e
Alena Bogdanova <itsmywork2018@yandex.ru>
2020-09-07 22:09:59 +0300

    лекция 4, шаги 6 и 7

---
+++ lesson4_step6.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/cats.html"
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    browser.find_element_by_id("button")
+
+finally:
+    time.sleep(5)
+    browser.quit()
